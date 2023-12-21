9101983537641b1d83037504f43f5cb79a07d634
Michael Lopes <gh api loopes.09@gmail.com>
2023-12-21 17:02:24 -0500

    Ajout d'une photo dans la navbar

--- src/sections/navbar/Navbar.jsx
+++ src/sections/navbar/Navbar.jsx
@@ -1,8 +1,24 @@
 import './navbar.css'
+import Logo from '../../assets/logo.jpg'
+import data from './data'
 
 const Navbar = () => {
   return (
-    <nav><h2>Navbar</h2></nav>
+    <nav>
+      <div className="container nav__container">
+        <a href="">
+          <img src={Logo} alt="Logo" />
+        </a>
+        <ul className='Nav__menu'>
+          {
+            data.map(item => <li><a href={item.link}>{item.title}</a></li> )
+          }
+
+
+        </ul>
+        <button></button>
+      </div>
+    </nav>
   )
 }
 
