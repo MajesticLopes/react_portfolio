177f38e08701e8b8687f2efcbea35f738e62323e
Michael Lopes <gh api loopes.09@gmail.com>
2023-12-26 17:39:07 -0500

    J'ai finalise le header et commence le About

--- src/sections/header/Header.jsx
+++ src/sections/header/Header.jsx
@@ -11,8 +11,6 @@ const Header = () => {
         <h3>Michael Lopes</h3>
         <p>
         Sports enthusiast and passionate about IT and sales, I am a junior full-stack developer with a solid background in management.
-        Having recently completed an intensive bootcamp at Le Wagon Montreal and having completed several team projects, I present myself as an adaptable, curious and very motivated person.
-        In short, feel comfortable asking me any questions regarding my professional background.
         </p>
         <div className='header__cta'>
           <a href="#contact" className='btn primary'>Let's Talk</a>
