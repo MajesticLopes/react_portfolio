7aae0e676b1d4605a46884e2b2c1469a3bb34515
Michael Lopes <gh api loopes.09@gmail.com>
2023-12-27 18:44:04 -0500

    Creation du data qui comprend la donnee de mes 2 projets

---
+++ src/sections/portfolio/data.js
@@ -0,0 +1,25 @@
+import Image1 from '../../assets/project1.png'
+import Image2 from '../../assets/project2.png'
+
+const  data = [
+  {
+    id: 1,
+    category: 'Full-Stack',
+    image: Image1,
+    title: "Plan-Eat (Full-Stack)",
+    desc: "With my team, I worked on the production of Plan-Eat, a mobile application that allows you to plan your meals for the week while remaining eco-responsible. Using Figma and the Agile methodology, we concluded this project after a period of 3 weeks in respecting the UI/UX design. The technical skills used are Ruby on Rails, SASS, Stimulus JS and Bootstrap.",
+    demo: "https://www.plan-eat.site/",
+    github: "https://github.com/Versus8/PLAN_EAT"
+
+  },
+  {
+    id: 2,
+    category: 'Full-Stack',
+    image: Image2,
+    title: "BookBnB (Full-Stack)",
+    desc: "This application takes a concept of book sales in which the user can not only buy but also sell his book. We also used Ruby on Rails, sass, and javascript.",
+    github: "https://github.com/Francoise1999/rails-BookBnB"
+  }
+]
+
+export default data
