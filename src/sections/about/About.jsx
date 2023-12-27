af9167c4bccf272e1bd6a2df13010f90c937492e
Michael Lopes <gh api loopes.09@gmail.com>
2023-12-26 19:16:14 -0500

    Ajustements de la section About

--- src/sections/about/About.jsx
+++ src/sections/about/About.jsx
@@ -2,6 +2,8 @@ import './about.css'
 import AboutImage from '../../assets/about.jpeg'
 import CV from '../../assets/cv.pdf'
 import { BsDownload } from "react-icons/bs";
+import Card from '../../components/Card'
+import data from './data'
 
 const About = () => {
   return (
@@ -9,12 +11,22 @@ const About = () => {
       <div className="container about__container">
         <div className="about__left">
           <div className="about__portrait">
-            <img src={AboutImage} alt="About Image" />
+            <img height="500" width="371" src={AboutImage} alt="About Image" />
           </div>
         </div>
         <div className="about__right">
           <h2>About me</h2>
-          <div className="about__cards"></div>
+          <div className="about__cards">
+            {
+              data.map(item => (
+                <Card key={item.id} className="about__card">
+                  <span className='about__card-icon'>{item.icon}</span>
+                  <h5>{item.title}</h5>
+                  <small>{item.desc}</small>
+                </Card>
+              ))
+            }
+          </div>
           <p>
             Having recently completed an intensive bootcamp at Le Wagon Montreal and having completed several team projects, I present myself as an adaptable, curious and very motivated person.
             In short, feel comfortable asking me any questions regarding my professional background. Check out my resume below !
